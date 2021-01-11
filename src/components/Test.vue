963cca08af69eb6e49d57f71209a320f07c9055b
ftn-Dentistry <77205775+ftn931652@users.noreply.github.com>
2021-01-12 02:14:40 +0800

    test1

---
+++ src/components/Test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="test">
+    <input type="text" v-model="text" @keyup.enter="addtask">
+    <button v-on:click="addtask">Submit</button>
+    <p v-for="(task,index) in todo_list" v-bind:key="index+'aaaaa'" >
+        {{task}}
+    </p>
+  </div>
+
+</template>
+
+<script>
+export default {
+name:"Test",
+data(){
+    return{
+        todo_list:[],
+        text:"",
+    }
+},
+methods:{
+   addtask(){
+       this.todo_list.push(this.text)
+       this.text=""
+   }
+}
+}
+</script>
+
+<style>
+
+</style>
